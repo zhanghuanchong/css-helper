1ae8a06b48a7f6f00df4af308b3e81e11d8a4aa5
Hans Zhang <devwingcom@gmail.com>
2016-07-01 11:29:18 +0800

    Init

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,30 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    util = require('gulp-util'),
+    del = require('del');
 
+gulp.task('clean', function() {
+    return del([
+        'dist/**/*.css'
+    ]);
+});
+
+gulp.task('less', function () {
+    var stream = gulp.src('src/css-helper.less')
+        .pipe(less());
+    stream.on('error', function (e) {
+        util.log(e);
+        stream.end();
+    });
+    return stream.pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['less'], function () {
+    gulp.watch('styles/less/*.less', ['less']);
+});
+
+gulp.task('build', ['clean'], function () {
+    gulp.start(['less']);
+});
+
+gulp.task('default', ['build']);
